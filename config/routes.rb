b4b198e1f3e701fbf6148210e833dad31c8a7e6d
github username <github email address>
2021-01-26 15:44:05 -0800

    commit tree 64c6ddd9c0a5a776d3259bc4da7926958cc1eb27

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  root 'welcome#home'
 end
